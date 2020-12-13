62da9f2219ae485600da9ada0e45b1ad3966a5a7
Ali Ünal <unal.ali.95@gmail.com>
2020-12-13 22:09:54 +0300

    Initial Commit

---
+++ ToDoApp-iOS13/Controller/TodoListViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ToDoApp-iOS13
+//
+//  Created by Ali Ünal on 13/12/2020.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    @IBOutlet weak var addItem: UIBarButtonItem!
+    var array = ["Ali", "Kardelen", "Melisa"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell?.accessoryType == .checkmark {
+            cell?.accessoryType = .none
+        }else{
+            cell?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+
+
+}
+
